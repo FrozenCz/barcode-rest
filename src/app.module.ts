f16f987a36072b1c21790dfc02f2dc9ee5dc13bc
FrozenCz <info@milanknop.cz>
2022-12-19 18:11:22 +0100

    add docker

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,18 +2,25 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: 'postgres',
-      password: 'postgres',
-      database: 'barcodetest',
-      entities: [__dirname + '/**/*.entity{.ts,.js}'],
-      synchronize: true,
+    ConfigModule.forRoot({ envFilePath: [`environments/.env.${process.env.STAGE}`] }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('HOST'),
+        port: +configService.get<number>('PORT'),
+        username: configService.get('DATABASE_USER'),
+        password: configService.get('DATABASE_PASSWORD'),
+        database: configService.get('DATABASE_NAME'),
+        schema: configService.get('DATABASE_SCHEMA'),
+        entities: [__dirname + '/**/*.entity.{ts,js}'],
+        synchronize: configService.get('SYNC'),
+      }),
+      inject: [ConfigService],
     }),
   ],
   controllers: [AppController],
